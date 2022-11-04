f6a70542b1681a0be0fdcbd1e2b51199ac5aaa28
Atharva Ranade <aranade@nbbj.com>
2022-11-04 10:17:06 -0700

    webpack

---
+++ 01_04_Webpack/04-webpack/src/script.js
@@ -0,0 +1,35 @@
+import './style.css'
+import * as THREE from 'three'
+
+//scene
+const scene = new THREE.Scene()
+
+//geometry - mesh --> red cube
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color: 'red'});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+//Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 2
+camera.position.z = 2
+scene.add(camera)
+
+// renderer
+const canvas = document.querySelector(".webgl")
+console.log(canvas)
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas    
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
+
